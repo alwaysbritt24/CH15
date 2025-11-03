f1656d4493bb34b68fb6ac07fb4c27bc27969a18
alwaysbritt24 <bslee05@icloud.com>
2025-11-03 11:09:07 -0500

    commit

--- Lootlogger/DetailViewController.swift
+++ Lootlogger/DetailViewController.swift
@@ -5,4 +5,38 @@
 //  Created by Brittany on 11/3/25.
 //
 
-import Foundation
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var serialNumberField: UITextField!
+    @IBOutlet var valueField: UITextField!
+    @IBOutlet var dateLabel: UILabel!
+    
+    var item: Item!
+    
+    let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }()
+
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter
+    }()
+
+      override func viewWillAppear(_ animated: Bool) {
+          super.viewWillAppear(animated)
+
+          nameField.text = item.name
+          serialNumberField.text = item.serialNumber
+          valueField.text = numberFormatter.string(from: NSNumber(value: item.valueInDollars))
+          dateLabel.text = dateFormatter.string(from: item.dateCreated)
+      }
+}
